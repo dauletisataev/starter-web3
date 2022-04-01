9eff3dca530c9d7536565a0b33a16cd7b4e5427a
iantanwx <iantanwx@bytedance.com>
2022-04-01 17:47:48 +0800

    feat: add a few minor improvements

--- packages/ui/src/components/Parking.tsx
+++ packages/ui/src/components/Parking.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useContract, useContractRead, useProvider } from "wagmi";
+import { useConnect, useContract, useProvider } from "wagmi";
 import ParkingLotToken from "@parking-web3/hardhat/artifacts/contracts/ParkingLotToken.sol/ParkingLotToken.json";
 
 function Parking() {
   const provider = useProvider();
+  const [connected] = useConnect();
   const contract = useContract({
-    addressOrName: "0x5fbdb2315678afecb367f032d93f642f64180aa3",
+    addressOrName: "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0",
     contractInterface: ParkingLotToken.abi,
     signerOrProvider: provider,
   });
@@ -14,9 +15,13 @@ function Parking() {
 
   useEffect(() => {
     (async () => {
-      setTotalSupply(await contract.totalSupply());
+      if (connected) {
+        const sup = await contract.totalSupply();
+        setTotalSupply(sup.toString());
+      }
     })();
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [connected]);
 
   return <div>Parking lots totalSupply: {totalSupply}</div>;
 }
