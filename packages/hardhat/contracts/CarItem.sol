c2546470dcfa32427fde5d13cbe2d5764159db98
daulet <daulet.issatayev@nu.edu.kz>
2022-04-06 12:18:26 +0600

    feat: ERC721Enumerable CarItem

--- packages/hardhat/contracts/CarItem.sol
+++ packages/hardhat/contracts/CarItem.sol
@@ -4,12 +4,13 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./ParkingLotToken.sol";
 import "./ParkingLot.sol";
 
-contract CarItem is ERC721URIStorage {
+contract CarItem is ERC721URIStorage, ERC721Enumerable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     ParkingLotToken private _plt;
@@ -33,6 +34,35 @@ contract CarItem is ERC721URIStorage {
 
     function approveFor(address pl) public {
         setApprovalForAll(pl, true);
+    }
+    
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 
     event Created(uint256 carId);
